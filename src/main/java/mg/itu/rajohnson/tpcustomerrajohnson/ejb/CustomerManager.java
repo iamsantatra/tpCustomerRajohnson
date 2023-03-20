b7defbe3dae2bb4122186a91baa6a9c0d598858c
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-03-20 19:39:58 +0300

    Ajout d'un lien dans la table pour déclencher l'affichage des détails d'un client 2.0

--- src/main/java/mg/itu/rajohnson/tpcustomerrajohnson/ejb/CustomerManager.java
+++ src/main/java/mg/itu/rajohnson/tpcustomerrajohnson/ejb/CustomerManager.java
@@ -20,20 +20,24 @@ import mg.itu.rajohnson.tpcustomerrajohnson.entities.Customer;
  */
 @Stateless
 public class CustomerManager {
-    
+
     @PersistenceContext(unitName = "customerPU")
     private EntityManager em;
 
     public List<Customer> getAllCustomers() {
-       Query query = em.createNamedQuery("Customer.findAll");
-       return query.getResultList();
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
     }
 
     public Customer update(Customer customer) {
-       return em.merge(customer);
+        return em.merge(customer);
     }
 
     public void persist(Customer customer) {
-       em.persist(customer);
+        em.persist(customer);
+    }
+
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
     }
 }

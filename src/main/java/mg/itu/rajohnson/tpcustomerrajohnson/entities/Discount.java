cac0a6e8aaa3d702ea76a23d6fbfff201f30da25
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-03-26 00:55:36 +0300

    Réductions par ordre croissant des taux de réduction

--- src/main/java/mg/itu/rajohnson/tpcustomerrajohnson/entities/Discount.java
+++ src/main/java/mg/itu/rajohnson/tpcustomerrajohnson/entities/Discount.java
@@ -26,7 +26,9 @@ import jakarta.persistence.Table;
 @NamedQueries({
     @NamedQuery(name = "Discount.findAll", query = "SELECT d FROM Discount d"),
     @NamedQuery(name = "Discount.findByCode", query = "SELECT d FROM Discount d WHERE d.code = :code"),
-    @NamedQuery(name = "Discount.findByRate", query = "SELECT d FROM Discount d WHERE d.rate = :rate")})
+    @NamedQuery(name = "Discount.findByRate", query = "SELECT d FROM Discount d WHERE d.rate = :rate"),
+    @NamedQuery(name = "Discount.findAllByRateAsc", query = "SELECT d FROM Discount d ORDER BY d.rate ASC")
+})
 public class Discount implements Serializable {
 
     private static final long serialVersionUID = 1L;
